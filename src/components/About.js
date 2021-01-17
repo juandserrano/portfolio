526a3d752683a671d658a3d3eb03756aa91cae7d
Juan Diego Serrano <juandserrano@gmail.com>
2021-01-17 13:57:00 -0500

    fixed photo about

--- src/components/About.js
+++ src/components/About.js
@@ -6,33 +6,40 @@ import {makeStyles} from "@material-ui/styles"
 
 const useStyles = makeStyles({
     mainContainer: {
-    paddingTop: "20px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: "5vw",
-    margin: "10vh 10vw 20vh 10vw",
-    borderRadius: "30px",
-    backgroundColor: "rgba(220,220,220,0.9)",
-    boxShadow: "2px 2px 5px black"
+        paddingTop: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "5vw",
+        margin: "10vh 10vw 20vh 10vw",
+        borderRadius: "30px",
+        backgroundColor: "rgba(220,220,220,0.9)",
+        boxShadow: "2px 2px 5px black"
     }
   });
 
 const About = () => {
     const classes = useStyles();
     return (
+        <div style={{display: "flex", minHeight: "100vh", alignItems: "center"}}>
         <Paper className={classes.mainContainer}>
             <h4 className="aboutTitle">About me</h4>
         <div className="mainInfo">
-        <img id="myPhoto" src={photo} />
-        
-        <p id="text">
-            Bachelor in Industrial Engineering from the Javeriana University with a Master’s degree in Logistics from the University Duisburg-Essen in Germany. Creative, honest, confident and with knowledge on systems and informatics, open minded and with a great ability to learn new things. With excellent English and basic German skills. I am known for being analytic, responsible, charismatic, leader, and have great relationships with the rest. Because of that, I am a very good teamwork member. Have substantial experience in Project management, Logistics, Productivity and Social Service & Responsibility. Currently I am the SAP Business Process Champion for the WM, OTC and BW modules for Latin America and process engineer in charge of productivity projects on the different distribution centers at Newell Rubbermaid. Specially focused in process optimization, productivity and warehouse supervision, layout design and slotting. Willing and eager to learn new things that help me as well as my company grow and improve myself every day.
-        </p>
+            <img id="myPhoto" src={photo} />
+            <div className="info" style={{display:"flex", flexDirection:"column", alignItems: "center"}}>
+                <p className="text">
+                    Hi There! I´m a web developer and your go-to IT guy. I´ve been teaching myself IT and coding since the early 2000s. I´m a Bachelor in Engineering with a Master´s in Logistics, 
+                    who in 2017 decide to pursue my passion as a career in the IT field.
+                </p>
+                <p className="text">
+                    Knowledge in AWS, Azure and Firebase, with a recent certification on Azure Fundamentals. Aside from IT knowledge, 
+                    I have substantial experience in Project management, Logistics and SAP. Willing and eager to learn new things that help me as well as my company grow and improve myself every day.
+                </p>
+            </div>
         </div>
         
         </Paper>
-        
+        </div>
     )
 }
 
