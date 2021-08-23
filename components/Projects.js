90beb2d782e3b0c660465237f6f23cffb10a90e3
Juan Diego Serrano <juandserrano@gmail.com>
2021-08-23 09:03:55 -0400

    changes projects to json

--- components/Projects.js
+++ components/Projects.js
@@ -1,21 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
 import ProjectCard from "./ProjectCard";
 import projects from '../pages/data/projects';
-import { useEffect } from "react";
-
 
 function Projects() {
    
     return (
         <div className="h-screen w-full projects-path py-56 px-12 pl-32 overflow-hidden" id="projects">
             <div className='w-full h-full flex items-center justify-center overflow-hidden relative'>
-                
-                    
                         {projects.map((item) => (
-                            
                                 <ProjectCard key={item.name} github={item.github} link={item.url} image={item.image} title={item.name} text={item.text}/>
-
-                            
                         ))}
                         <div className='absolute flex justify-center items-center w-10 h-10 left-0 bg-gray-200 bg-opacity-30 rounded-full cursor-pointer hover:text-white select-none'>
                             a
@@ -23,14 +16,7 @@ function Projects() {
                         <div className='absolute flex justify-center items-center w-10 h-10 right-0 bg-gray-200 bg-opacity-30 rounded-full cursor-pointer hover:text-white select-none'>
                             b
                         </div>
-
-                
             </div>
-
-
-            
-
-            
         </div>
     )
 }
