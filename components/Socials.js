d0c98887059231930825d0cd26a30fd8d95c8659
Juan Diego Serrano <juandserrano@gmail.com>
2021-08-23 09:34:57 -0400

    fixed fa icons

--- components/Socials.js
+++ components/Socials.js
@@ -6,27 +6,27 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 function Socials() {
   return (
     <div
-      className="h-screen w-full bg-[rgb(11,22,40)] flex items-center justify-center space-x-40 pl-20 text-yellow-400 z-40"
+      className="h-screen w-full bg-[rgb(11,22,40)] border border-white flex items-center justify-center space-x-40 pl-20 text-yellow-400 z-40"
       id="socials"
     >
       <a href="https://github.com/juandserrano">
         <FontAwesomeIcon
           className="hover:animate-pulse cursor-pointer active:scale-95 active:text-yellow-500"
-          height="80"
+          size="6x"
           icon={faGithub}
         />
       </a>
       <a href="https://linkedin.com/in/juandserrano">
         <FontAwesomeIcon
           className="hover:animate-pulse cursor-pointer active:scale-95 active:text-yellow-500"
-          height="80"
+          size="6x"
           icon={faLinkedin}
         />
       </a>
       <a href="mailto:juandserrano@gmail.com">
         <FontAwesomeIcon
           className="hover:animate-pulse cursor-pointer active:scale-95 active:text-yellow-500"
-          height="80"
+          size="6x"
           icon={faEnvelope}
         />
       </a>
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <title>Juan Diego Serrano - Developer</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet" />
       </Head>
       <div className='flex w-full relative'>
