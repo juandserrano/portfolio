f4179abe924da8220b252f5af9092d9aae7601de
Juan Diego Serrano <juandserrano@gmail.com>
2021-08-29 10:15:26 -0400

    added title to projects card

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
-function ProjectCard({ image, text, link, github }) {
+function ProjectCard({ title, image, text, link, github }) {
   return (
     <div className="w-[300px] md:w-[600px] mx-5 md:h-[650px] p-8 frosty shadow-lg rounded-3xl flex flex-col items-center justify-around flex-shrink-0 md:pt-16">
       <div className="relative w-11/12 h-[160px] md:h-4/6 border-0 border-yellow-400">
+        <div className='uppercase text-center text-white mb-4 underline text-xl'>{title}</div>
         <img src={image} className='absolute rounded-xl h-full w-full'/>
       </div>
       <div className="flex flex-col h-2/6 items-center text-white">
-        <p className="p-3 mb-10 myTextShadow text-lg text-center">{text}</p>
+        <p className="p-3 mb-10 myTextShadow text-lg text-center">{/*text*/}</p>
         <div className="flex space-x-5">
           <a
             className="border border-yellow-400 bg-yellow-400 shadow-md text-[rgb(11,22,40)] font-semibold py-3 px-5 rounded-xl hover:scale-105 active:scale-95 hover:shadow-md cursor-pointer"
