e04e60582dbfb583b0f1d51240f5659950332a38
Juan Diego Serrano <juandserrano@gmail.com>
2021-09-03 13:18:46 -0400

    fixed hero image size

--- components/Hero.js
+++ components/Hero.js
@@ -12,7 +12,7 @@ function Hero() {
   return (
     
       <div className="flex flex-col w-full h-screen bg-[rgb(11,22,40)] text-gray-200 items-center justify-center" id="main">
-        <div className='mb-10 border-4 rounded-full w-40 h-40 md:w-80 md:h-80 relative border-yellow-400'>
+        <div className='mb-10 mt-5 border-4 rounded-full w-40 h-40 md:w-[40vh] md:h-[40vh] relative border-yellow-400'>
           <img src='/images/Foto3.jpg' className='rounded-full h-full w-full'/>
         </div>
         
@@ -24,7 +24,7 @@ function Hero() {
           Hello there! My name is Juan and I&apos;m an Ottawa-based Developer. I have Experience in HTML/CSS/JS, React, RESTful APIs,
           Express, Java and in working with Pandas and TensorFlow for Data Analysis and Machine Learning, among other supporting technologies. 
         </p>
-        <div className='text-2xl md:text-3xl mt-10 text-yellow-400'>
+        <div className='text-2xl md:text-3xl mt-10 text-yellow-400 mb-10'>
           <TypeIt
             options={{
               strings: ["Frontend", "Backend", "Java", "Python", "Databases"],
