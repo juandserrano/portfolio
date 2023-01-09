ad234570e0172bf541191b2515e88a02fd42650f
Juan Diego Serrano <juandserrano@gmail.com>
2023-01-08 19:54:26 -0500

    Add gradient to projects

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/react-in-jsx-scope */
 import ProjectCard from "./ProjectCard";
+import ProjectHero from "./ProjectHero";
 import projects from "../pages/data/projects";
 import { useState } from "react";
 import {
@@ -25,14 +26,14 @@ function Projects() {
 
   return (
     <div
-      className="h-screen w-full projects-path md:px-8 overflow-hidden"
+      className="relative h-screen w-full projects-path md:px-8 overflow-hidden"
       id="projects"
     >
       <div className="w-full h-full flex items-center justify-center overflow-visible relative">
         {projects.map((item, index) => (
             <div className={index === current ? 'slide active flex items-center relative h-full' : 'slide flex relative'} key={index}>
                 {index === current && (
-                    <ProjectCard
+                    <ProjectHero
                       github={item.github}
                       link={item.url}
                       image={item.image}
@@ -44,10 +45,10 @@ function Projects() {
             </div>
                 
         ))}
-        <div onClick={prevSlide} className="absolute flex text-gray-400 justify-center items-center hover:animate-ping w-10 h-10 md:w-16 md:h-16 left-4 bg-gray-200 bg-opacity-30 rounded-full cursor-pointer hover:text-white select-none">
+        <div onClick={prevSlide} className="absolute z-20 flex text-gray-400 justify-center items-center hover:animate-ping w-10 h-10 md:w-16 md:h-16 left-14 bg-gray-200 bg-opacity-30 rounded-full cursor-pointer hover:text-white select-none">
           <FontAwesomeIcon icon={faChevronLeft} size="x" />
         </div>
-        <div onClick={nextSlide} className="absolute flex text-gray-400 justify-center items-center hover:animate-ping w-10 h-10 md:w-16 md:h-16 right-4 bg-gray-200 bg-opacity-30 rounded-full cursor-pointer hover:text-white select-none">
+        <div onClick={nextSlide} className="absolute z-20 flex text-gray-400 justify-center items-center hover:animate-ping w-10 h-10 md:w-16 md:h-16 right-4 bg-gray-200 bg-opacity-30 rounded-full cursor-pointer hover:text-white select-none">
           <FontAwesomeIcon icon={faChevronRight} size="x" />
         </div>
       </div>
